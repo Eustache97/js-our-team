9d976ba34c0a78a7c943dd6a7b14e606727979cb
Eustache97 <eubomber97@gmail.com>
2022-10-20 20:53:11 +0200

    bonus1 & bonus2

--- js/script.js
+++ js/script.js
@@ -45,11 +45,36 @@ for(let i = 0; i < teamMembersArray.length; i++){
 }
 //Milestone2--Prendiamo l'elemento del DOM che ci serve e col ciclo for ci stampiamo le info dei membri
 const listMembers = document.querySelector(".list-team");
-for(let i = 0; i < teamMembersArray.length; i++){
-    const element = teamMembersArray[i];
-    listMembers.innerHTML += `<tr>
-                                <td>${element.name}</td>
-                                <td>${element.role}</td>
-                                <td>${element.photo}</td>
-                            </tr>`;
+// for(let i = 0; i < teamMembersArray.length; i++){
+//     const element = teamMembersArray[i];
+//     listMembers.innerHTML += `<tr>
+//                                 <td>${element.name}</td>
+//                                 <td>${element.role}</td>
+//                                 <td>${element.photo}</td>
+//                             </tr>`;
+// }
+
+
+// Prendiamo il container dal DOM
+const container = document.querySelector(".container");
+//creiamo l'elemento row
+const cardsRow = document.createElement("div");
+cardsRow.classList.add("my_row");
+// richiamo la funzione per creare le card
+const appendElement = createCard(teamMembersArray, cardsRow, container);
+
+//funzione per creare una card
+function createCard(array, row, contenitor){
+    for(let i = 0;i < array.length; i++){
+        const element = array[i];
+        const card = document.createElement("div");
+    card.classList.add("my_col");
+    card.innerHTML += `<img src="img/${element.photo}" alt="">
+                        <h3>${element.name}</h3>
+                        <p>${element.role}</p>`;
+    row.append(card);
+    contenitor.append(cardsRow);
+    
+}
+    
 }
